0ba834e458878c69a3b38567d350cde6408c0344
EvenDraco <157450729+EvenDraco@users.noreply.github.com>
2024-01-22 22:12:51 -0800

    Lab Assignment Percentages

---
+++ Lab_Assignment_percentages_Evelynn_Draconis/main.cpp
@@ -0,0 +1,36 @@
+// Evelynn Draconis
+// 1/08/2024
+//system Libs
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+
+//user libraries
+
+//global constants - Math, Physics, Chemistry, Conversions
+
+//function prototypes
+
+//program execution begins here
+
+int main() {
+    //set Rand Seed
+    
+    //declare all Var. Less than 7 char.
+     float milBdgt,fedBdgt,mlPrcnt;
+
+    //initialize all Var.
+     milBdgt=7.0e11f;    //Military Budget = 700 Billion   
+
+     fedBdgt=4.1e12f;    //Federal Budget  = 4.1 Trillion
+                
+    //process or map solutions
+    mlPrcnt = milBdgt/fedBdgt;
+    //display output
+    cout << "The percentage the U.S.A. government spent on the military in 2018 was approximately " <<
+            fixed << setprecision(2)<<mlPrcnt*100 << "%." << endl;
+    //exit program
+
+    return 0;
+}
